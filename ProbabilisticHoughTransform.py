c6ef7f82560542f8d7ea4fe37b71ddea2aa31758
Yoo <sangduk.yoo@hpe.com>
2020-06-10 20:34:13 +0900

    modify file

--- ProbabilisticHoughTransform.py
+++ ProbabilisticHoughTransform.py
@@ -7,7 +7,7 @@ def show_ProbabilisticHoughTransform():
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray,50,150,apertureSize = 3)
     minLineLength = 50
-    maxLineGap = 10
+    maxLineGap = 5
 
     lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
 
